cb0f50d6e8175d9dda84d8462b100d96efffbe72
Adam Csapo <csapo.adam@gmail.com>
2020-10-26 11:15:45 +0100

    miert van szukseg destruktorra?

--- gyak-03/main.cpp
+++ gyak-03/main.cpp
@@ -4,46 +4,17 @@
 #include <iostream>
 #include "LinkedList.h"
 
-int main()
-{
+void f() {
     LifoList myLifoList;
-    myLifoList.print();
     myLifoList.addNode(5).addNode(10).addNode(12);
     myLifoList.print();
+}
 
-    // TODO valahogy...
-    LifoList myLifoList2(myLifoList);
-    myLifoList2.print();
-
-    myLifoList.addNode(15);
-    myLifoList.print();
-    myLifoList2.print();
-
-    LinkedList myLinkedList;
-    myLinkedList.print();
-    myLinkedList.addNode(5).addNode(10).addNode(12);
-    myLinkedList.print();
-
-    std::cout << "----" << std::endl;
-    LinkedList myLinkedList2(myLinkedList);
-    myLinkedList2.print();
-    myLinkedList.addNode(15);
-    myLinkedList.print();
-    myLinkedList2.print();
-
-    myLinkedList2.addNode(17);
-    myLinkedList.print();
-    myLinkedList2.print();
-
-    std::cout << "Toroljuk a myLinkedList2 elso elemet" << std::endl;
-    myLinkedList2.deleteItem(1);
-    myLinkedList2.print();
-
-    std::cout << "Toroljuk az uj myLinkedList2 4. elemet" << std::endl;
-    myLinkedList2.deleteItem(4);
-    myLinkedList2.print();
-
-    std::cout << "Toroljuk az uj myLinkedList2 3. elemet" << std::endl;
-    myLinkedList2.deleteItem(3);
-    myLinkedList2.print();
+int main()
+{
+    char c;
+    for (int i = 0; i < 10000; i++) {
+        f();
+        std::cin >> c;
+    }
 }
